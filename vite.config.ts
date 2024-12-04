f38db080b5c3e0d650ac2e93020c3567a96a783a
Samuel Llibre-Pillco <dev@zyruks.com>
2024-12-03 21:37:03 -0400

    chore(config): 🔧 update Vite config and tsconfig for path aliases
    Updated:
    - `vite.config.ts` to include path aliases for better imports
    - `tsconfig.app.json` to align with Vite's alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/views'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@api': path.resolve(__dirname, './src/api'),
+    },
+  }
+});
