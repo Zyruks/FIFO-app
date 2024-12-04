5424dabfddbc61d9923920c36308339e2742c985
Samuel Llibre-Pillco <dev@zyruks.com>
2024-12-03 22:03:16 -0400

    feat(app): ✨ initialize main.tsx with providers and App component
    Added:
    - `main.tsx` to render the application with `AuthProvider`, `QueueProvider`, and `ThemeProvider`
    - Includes `StrictMode` for development checks
    - Imports global styles from `main.scss`

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import App from './app/App.tsx';
+import { AuthProvider, QueueProvider, ThemeProvider } from '@common';
+import { createRoot } from 'react-dom/client';
+
+import '@styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <QueueProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </QueueProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
